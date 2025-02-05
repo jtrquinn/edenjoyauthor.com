9056dfa168fe1202226f6b9ac178711081833b5a
Jack Quinn <jtrquinn@gmail.com>
2025-02-05 13:41:06 -0500

    updated homepage html

--- page-home.php
+++ page-home.php
@@ -109,9 +109,55 @@
 					<div class="flex flex-col mx-auto max-w-xl gap-y-20 lg:max-w-none">
 						<div class="flex flex-col gap-16">
 							<div class="flex">
-								<div class="flex justify-center items-center pe-12">
+								<div class="hidden sm:flex justify-center items-center pe-4 md:pe-12">
 									<img src="https://cdn.collider.com/wp-content/uploads/2016/07/the-lord-of-the-rings-book-cover.jpg"
-										alt="" class="w-[30rem]">
+										alt="" class="w-[16rem] md:w-[30rem] min-w-[6.75rem]">
+								</div>
+								<article class="group relative flex flex-col justify-center items-start">
+									<h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+										<div
+											class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50">
+										</div>
+										<a href="/articles/crafting-a-design-system-for-a-multiplanetary-future">
+											<span
+												class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+											<span class="relative z-10">
+												Voices of the Future: Stories of Bravery & Belonging
+											</span>
+										</a>
+									</h2>
+									<time
+										class="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"
+										datetime="2022-09-05">
+										<span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
+											<span class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
+										</span>
+										February 25, 2025
+									</time>
+									<p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+										The fifth student showcase anthology from The Author Conservatory, a
+										college-alternative program for gifted writers focused on raising up the next
+										C.S.
+										Lewis and J.R.R. Tolkien. 100% of your purchase goes to help students attend
+										writing
+										conferences where they can pitch their work to agents and publishers.
+									</p>
+									<div aria-hidden="true"
+										class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">
+										Go to Book Page
+										<svg viewBox="0 0 16 16" fill="none" aria-hidden="true"
+											class="ml-1 h-4 w-4 stroke-current">
+											<path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5"
+												stroke-linecap="round" stroke-linejoin="round">
+											</path>
+										</svg>
+									</div>
+								</article>
+							</div>
+							<div class="flex">
+								<div class="hidden sm:flex justify-center items-center pe-4 md:pe-12">
+									<img src="https://cdn.collider.com/wp-content/uploads/2016/07/the-lord-of-the-rings-book-cover.jpg"
+										alt="" class="w-[16rem] md:w-[30rem] min-w-[6.75rem]">
 								</div>
 								<article class="group relative flex flex-col justify-center items-start">
 									<h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
